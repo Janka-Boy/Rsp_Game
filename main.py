61a0fd720903de3a9cfcc5ac5a6513b45e2c7dc9
JanKa <56315940+Janka-Boy@users.noreply.github.com>
2021-10-07 06:13:19 +0000

    bruh

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import random
 
 turns = ["rock", "paper", "scissors"]
-computer_turn = random.choice(turns)
-human_turn = input("Enter human turn: ").lower()
 
-print(f"Computer: {computer_turn} VS. Human: {human_turn}")
 
-if computer_turn == human_turn:
-    print("Draw!")
+while True:
+    computer_turn = random.choice(turns)
 
-elif ((computer_turn == "paper" and human_turn == "rock") or 
-        (computer_turn == "scissors" and human_turn == "paper") or 
-        (computer_turn == "rock" and human_turn == "scissors")):
-    print("Computer wins!")
+    while True:
+        human_turn = input("Enter human turn: ").lower()
+        if human_turn == "rock" or human_turn == "paper" or human_turn == "scissors":
+            break
+        else:
+            print("Select only from rock, paper or scissors")
 
-else:
-    print("Human wins!")
+    print(f"Computer: {computer_turn} VS. Human: {human_turn}")
+
+    if computer_turn == human_turn:
+        print("Draw!")
+
+    elif ((computer_turn == "paper" and human_turn == "rock") or 
+            (computer_turn == "scissors" and human_turn == "paper") or 
+            (computer_turn == "rock" and human_turn == "scissors")):
+        print("Computer wins!")
+
+    else:
+        print("Human wins!")
